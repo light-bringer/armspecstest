eeaf85189764a108737cb5764d9df2f8e7304fa4
Azhor Ahai <debapriya.das@incture.com>
2017-01-12 01:25:16 +0530

    adding a build failure test

--- localTest.cmd
+++ localTest.cmd
@@ -5,13 +5,13 @@ REM The number of tests to run in parallel
 set PARALLEL_DEPLOYMENT_NUMBER=6
 
 REM The endpoint that our validate and deploy requests will be sent to
-set VALIDATION_HOST=http://qst9494f4f5c44dd94d84049.westus.cloudapp.azure.com
+set VALIDATION_HOST=http://aarm-server.cloudapp.net:30000
 
 REM Whether to skip the remote validate test or not.
-set VALIDATION_SKIP_VALIDATE=true
+set VALIDATION_SKIP_VALIDATE=false
 
 REM Whether to skip the remote deploy test or not.
-set VALIDATION_SKIP_DEPLOY=true
+set VALIDATION_SKIP_DEPLOY=false
 
 call mocha
 endlocal
